fd4d06ac9e72c683c8aae4f0fa529f3145169380
Jip <jhp.moors@gmail.com>
2023-12-06 09:56:13 +0100

    Just the same code..
    Only the input was tweaked a bit

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -27,24 +27,7 @@ class Day6 extends Day {
   }
 
   solveForPartTwo (input: string): string {
-    const lines = input.split('\n').filter(a => a)
-
-    const durations = [...lines[0].matchAll(/\d+/g)].map(result => parseInt(result[0]))
-    const distances = [...lines[1].matchAll(/\d+/g)].map(result => parseInt(result[0]))
-
-    const winners: number[] = []
-
-    durations.forEach((duration, index) => {
-      winners[index] = 0
-      const distanceToBeat = distances[index]
-      for (let d = 1; d < duration - 1; d++) {
-        if (d * (duration - d) > distanceToBeat) {
-          winners[index]++
-        }
-      }
-    })
-
-    return winners.reduce((total, number) => total * number, 1).toString()
+    return this.solveForPartOne(input)
   }
 }
 
