f44076290701809d0b0667c3b97e829a296e6e18
Jip <jhp.moors@gmail.com>
2023-12-09 10:28:04 +0100

    Day 10 setup

---
+++ src/day10/day10.spec.ts
@@ -0,0 +1,8 @@
+import day10 from './index'
+
+describe('On Day 10', () => {
+  it('part1 is identity function', () => {
+    expect(day10.solveForPartOne('hello')).toBe('hello')
+    expect(day10.solveForPartTwo('hello')).toBe('hello')
+  })
+})
