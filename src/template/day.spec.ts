e594f026f96b95c517322b2f2365b5857c68bb86
Jip <jhp.moors@gmail.com>
2023-12-10 20:42:56 +0100

    Better test template

--- src/template/day.spec.ts
+++ src/template/day.spec.ts
@@ -1,8 +1,10 @@
 import day0 from './index'
 
 describe('On Day 0', () => {
-  it('part1 is identity function', () => {
+  it('part1', () => {
     expect(day0.solveForPartOne('hello')).toBe('hello')
+  })
+  it('part2', () => {
     expect(day0.solveForPartTwo('hello')).toBe('hello')
   })
 })
